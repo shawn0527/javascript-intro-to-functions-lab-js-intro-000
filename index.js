cb4f6a3951a7b25e2f299a5a2215b96b8ce9265d
Shixiang Ren <doggy-ren@hotmail.com>
2019-01-29 03:19:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
+
+var lowercase='hello!'
+
+function sayHiToGrandma(string){
+  if string.toLowerCase()===lowercase{
+    return "I can't hear you!"
+  }
+}
